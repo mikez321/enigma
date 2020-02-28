41fd06357ceeb76cd0edb478a75394a3f446d115
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 12:24:30 -0700

    Remove setup in test for easier test creation

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,17 +4,17 @@ require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
 
-  def setup
-    @enigma = Enigma.new("message", "key", "date")
-  end
-
   def test_it_exists
-    assert_instance_of Enigma, @enigma
+    enigma = Enigma.new("message", "key", "date")
+    assert_instance_of Enigma, enigma
   end
 
   def test_it_has_information
-    assert_equal "message", @enigma.message
-    assert_equal "key", @enigma.key
-    assert_equal "date", @enigma.date
+    enigma = Enigma.new("message", "key", "date")
+    assert_equal "message", enigma.message
+    assert_equal "key", enigma.key
+    assert_equal "date", enigma.date
   end
+
+  
 end
