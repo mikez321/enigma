b624373d70c974b7b7f1c3471fbe26d5c451b349
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 11:31:19 -0700

    Begin building filehandler class

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -1,6 +1,12 @@
 class FileHandler
-  attr_reader :file
+  attr_reader :file, :message
   def initialize(file)
     @file = file
+    @message = nil
   end
+
+def create_message
+  @message = File.open(file).read
+end
+
 end
