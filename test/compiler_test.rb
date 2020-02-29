a6d012f8ac9f7d9cb4610e26fc5999c80393c129
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 22:22:34 -0700

    Create test for breaking up messages

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -3,11 +3,13 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/compiler'
+require './lib/shifter'
 
 class CompilerTest < Minitest::Test
 
   def setup
     @compiler = Compiler.new("hello world")
+    @shifter = Shifter.new("02715", "040895")
   end
 
   def test_it_exists
@@ -16,5 +18,17 @@ class CompilerTest < Minitest::Test
 
   def test_it_has_a_message
     assert_equal "hello world", @compiler.message
+  end
+
+  def test_it_knows_the_alphabet
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, @compiler.alphabet
+  end
+
+  def test_it_handles_messages_in_blocks_of_four
+    expected = [['h', 'e', 'l', 'l'],['o', ' ', 'w', 'o'],['r', 'l', 'd']]
+    assert_equal expected, @compiler.deconstruct(@compiler.message)
   end
 end
