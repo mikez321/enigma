3b84c5c2f4e3441c5015af4ab721b336f0945627
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-29 11:11:23 -0700

    Remove setup add tests for default vaues  test for random number as key

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,17 +5,17 @@ require 'minitest/pride'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
-  def setup
-    @enigma = Enigma.new("message", "key", "date")
-  end
+  
   def test_it_exists
-    assert_instance_of Enigma, @enigma
+    enigma = Enigma.new("message", "key", "date")
+    assert_instance_of Enigma, enigma
   end
 
   def test_it_has_information
-    assert_equal "message", @enigma.message
-    assert_equal "key", @enigma.key
-    assert_equal "date", @enigma.date
+    enigma = Enigma.new("message", "key", "date")
+    assert_equal "message", enigma.message
+    assert_equal "key", enigma.key
+    assert_equal "date", enigma.date
   end
 
   def test_it_has_default_values
@@ -23,6 +23,15 @@ class EnigmaTest < Minitest::Test
   assert_equal "Message to Encrypt", enigma1.message
   assert_equal "default", enigma1.key
   assert_equal "000000", enigma1.date
+  end
+
+  def test_it_will_generate_a_random_five_digit_key
+    enigma1 = Enigma.new("Message to Encrypt")
+
+    assert_equal true, enigma1.key.length == 5
+    assert_equal true, enigma1.key >= 0
+    assert_equal true, enigma1.key <= 99999
+
   end
 
 end
