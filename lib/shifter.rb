c6c4a5d2beeac51b993bf72522a35f4db2db640b
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-29 12:36:22 -0700

    Rename variables

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -7,26 +7,27 @@ class Shifter
     @date = date
   end
 
-  def create_keys
-    keys = []
+  def create_key_shift
+    key_shift = []
     @key.chars.each_with_index do |num, index|
       if index < (@key.chars.length) - 1
-      keys << (num + @key.chars[index + 1]).to_i
+      key_shift << (num + @key.chars[index + 1]).to_i
       end
     end
-    keys
+    key_shift
   end
 
-  def create_shift
-    (@date.to_i ** 2).to_s.chars.map { |num| num.to_i }[-4..-1]
+  def create_date_shift
+    date_squared = (@date.to_i ** 2).to_s
+    date_squared.chars.map { |num| num.to_i }[-4..-1]
   end
 
-  def total_shift
-    shifts = create_keys.zip(create_shift).map do |pair|
+  def compiler_ready_shift
+    summed_shifts = create_key_shift.zip(create_date_shift).map do |pair|
       pair.sum
     end
-    needed_letters = alphabet[0..(shifts.length-1)]
-    reference_shifts = Hash[needed_letters.zip(shifts)]
+    reference_letters = alphabet[0..(summed_shifts.length-1)]
+    compiler_shift = Hash[reference_letters.zip(summed_shifts)]
   end
 
 end
