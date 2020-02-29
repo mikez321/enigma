9a4849f3199650c2a4af9b2e6e6d9ab1f3ad1279
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 19:35:08 -0700

    Create method for creating keys

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,5 +4,19 @@ class Enigma
     @message = message
     @key = key
     @date = date
+    @alphabet= ("a".."z").to_a
   end
+
+  def create_keys
+    {"A" => (@key[0] + @key[1]).to_i,
+     "B" => (@key[1] + @key[2]).to_i,
+     "C" => (@key[2] + @key[3]).to_i,
+     "D" => (@key[3] + @key[4]).to_i
+   }
+  end
+
+  def create_offset
+
+  end
+
 end
