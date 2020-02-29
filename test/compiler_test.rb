a43180b2c417be8e65b0e5f751040344fdcbc768
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-29 12:54:50 -0700

    Removed setup and added a real shift to tests

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -7,48 +7,70 @@ require './lib/compiler'
 class CompilerTest < Minitest::Test
 
   def setup
-    @compiler = Compiler.new("hello world", "placeholder_shift")
+    @compiler_shift = {
+      "a" => 3,
+      "b" => 27,
+      "c" => 73,
+      "d" => 20
+    }
   end
 
   def test_it_exists
-    assert_instance_of Compiler, @compiler
+    compiler = Compiler.new("hello world", @compiler_shift)
+    assert_instance_of Compiler, compiler
   end
 
-  def test_it_has_a_message
-    assert_equal "hello world", @compiler.message
+  def test_it_has_a_message_and_a_compiler_ready_shift
+    compiler = Compiler.new("hello world", @compiler_shift)
+    assert_equal "hello world", compiler.message
+
+    expected = {
+      "a" => 3,
+      "b" => 27,
+      "c" => 73,
+      "d" => 20
+    }
+
+    assert_equal expected, compiler.total_shift
   end
 
   def test_it_knows_the_alphabet
+    compiler = Compiler.new("hello world", @compiler_shift)
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
-    assert_equal expected, @compiler.alphabet
+    assert_equal expected, compiler.alphabet
   end
 
   def test_it_knows_number_represenatations_of_letters
-    assert_equal 3, @compiler.digitize("d")
-    assert_equal 10, @compiler.digitize("K")
-    assert_equal 26, @compiler.digitize(" ")
+    compiler = Compiler.new("hello world", @compiler_shift)
+    assert_equal 3, compiler.digitize("d")
+    assert_equal 10, compiler.digitize("K")
+    assert_equal 26, compiler.digitize(" ")
   end
 
   def test_it_knows_letter_representations_of_numbers
-    assert_equal "d", @compiler.letterize(3)
-    assert_equal "k", @compiler.letterize(10)
-    assert_equal " ", @compiler.letterize(26)
+    compiler = Compiler.new("hello world", @compiler_shift)
+    assert_equal "d", compiler.letterize(3)
+    assert_equal "k", compiler.letterize(10)
+    assert_equal " ", compiler.letterize(26)
   end
 
   def test_it_can_scramble_by_an_offset_amount
-    assert_equal "f", @compiler.offset("d", 2)
-    assert_equal "m", @compiler.offset("h", 5)
-    assert_equal "a", @compiler.offset("z", 2)
+    compiler = Compiler.new("hello world", @compiler_shift)
+    assert_equal "f", compiler.offset("d", 2)
+    assert_equal "m", compiler.offset("h", 5)
+    assert_equal "a", compiler.offset("z", 2)
   end
 
   def test_it_handles_messages_in_blocks_of_four
+    compiler = Compiler.new("hello world", @compiler_shift)
     expected = [['h', 'e', 'l', 'l'],['o', ' ', 'w', 'o'],['r', 'l', 'd']]
-    assert_equal expected, @compiler.deconstruct(@compiler.message)
+    assert_equal expected, compiler.deconstruct(compiler.message)
   end
 
   def test_it_scrambles_and_returns_a_string
+    skip
     shift = {
       "a" => 3,
       "b" => 27,
