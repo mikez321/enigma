04864d28b4275511f67f4a5b5d5c269d296739fc
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 19:59:25 -0700

    Delete methods out of enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,16 +7,4 @@ class Enigma
     @alphabet= ("a".."z").to_a
   end
 
-  def create_keys
-    {"A" => (@key[0] + @key[1]).to_i,
-     "B" => (@key[1] + @key[2]).to_i,
-     "C" => (@key[2] + @key[3]).to_i,
-     "D" => (@key[3] + @key[4]).to_i
-   }
-  end
-
-  def create_offset
-
-  end
-
 end
