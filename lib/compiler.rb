589f92769acb40d989ae129785990ef3ea5abfdc
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 23:24:52 -0700

    Set helper methods to only operate on one characater

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -5,21 +5,25 @@ class Compiler
     @alphabet= ("a".."z").to_a << " "
   end
 
-  def digitize(word)
-    word.downcase.chars.map { |letter| @alphabet.index(letter) }
+  def digitize(letter)
+    @alphabet.index(letter.downcase)
   end
 
   def letterize(num)
     @alphabet[num]
   end
 
-  def scramble(word, offset)
-    digitize(word).map do |num|
-      letterize(num + offset)
-    end
+  def offset(letter, offset)
+      letterize((digitize(letter) + offset) % 27)
   end
 
   def deconstruct(word)
     word.chars.each_slice(4).map { |letter| letter.push }
   end
+
+  # def offset(word, offset)
+  #   digitize(word).map do |num|
+  #     letterize(num + offset)
+  #   end
+  # end
 end
