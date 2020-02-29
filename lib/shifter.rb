d7810a9b873c734438e569c9bbf9459f1a5382fc
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 22:28:45 -0700

    Remove unnecessary local variable

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -6,11 +6,10 @@ class Shifter
   end
 
   def create_keys
-    keys_from_input = @key.chars
     keys = []
-    keys_from_input.each_with_index do |num, index|
-      if index < (keys_from_input.length) - 1
-      keys << (num + keys_from_input[index + 1]).to_i
+    @key.chars.each_with_index do |num, index|
+      if index < (@key.chars.length) - 1
+      keys << (num + @key.chars[index + 1]).to_i
       end
     end
     keys
