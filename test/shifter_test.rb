fade36c6b0d3386a007ac395c9461ec39ed82bad
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 20:26:19 -0700

    Create test for key creation

---
+++ test/shifter_test.rb
@@ -0,0 +1,25 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/shifter'
+
+class ShifterTest < Minitest::Test
+
+  def setup
+    @shifter = Shifter.new("02715", "040895")
+  end
+
+  def test_it_exists
+    assert_instance_of Shifter, @shifter
+  end
+
+  def test_it_has_a_key_and_a_date
+    assert_equal "02715", @shifter.key
+    assert_equal "040895", @shifter.date
+  end
+
+  def test_it_can_create_keys
+    assert_equal [2, 27, 71, 15], @shifter.create_keys
+  end
+end
