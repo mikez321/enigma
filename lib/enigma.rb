827f8f1b4d7f4d4befa0935ea96a1371cfc46f25
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-29 13:57:07 -0700

    Create decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,10 +7,28 @@ class Enigma
     shifter = Shifter.new(key, date)
     compiler_shift = shifter.compiler_ready_shift
     compiler = Compiler.new(message, compiler_shift)
+    cipher = compiler.scramble
+
+    output = {
+      encryption: cipher,
+      key: key,
+      date: date
+    }
+  end
+
+  def decrypt(ciphertext, key, date)
+    shifter = Shifter.new(key, date)
+    
+    ## here's the difference
+    compiler_shift = shifter.compiler_ready_shift.transform_values do
+       |value| value * -1
+     end
+
+    compiler = Compiler.new(ciphertext, compiler_shift)
     message = compiler.scramble
 
     output = {
-      encryption: message,
+      decryption: message,
       key: key,
       date: date
     }
