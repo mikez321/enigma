636ac78195e9892a32d04d15f1cee259b8ab4b1d
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 22:24:14 -0700

    Create deconstruct method

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -1,6 +1,12 @@
 class Compiler
-  attr_reader :message
+  attr_reader :message, :alphabet
   def initialize(message)
     @message = message
+    @alphabet= ("a".."z").to_a << " "
   end
+
+  def deconstruct(word)
+    word.chars.each_slice(4).map { |letter| letter.push }
+  end
+
 end
