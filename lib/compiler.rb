d8ec9448a07717fc7033f865565edefb8fc18827
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 22:54:43 -0700

    Create letterize method

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -9,6 +9,16 @@ class Compiler
     word.downcase.chars.map { |letter| @alphabet.index(letter) }
   end
 
+  def letterize(num)
+    @alphabet[num]
+  end
+
+  def scramble(word, offset)
+    digitize(word).map do |num|
+      letterize(num + offset)
+    end
+  end
+
   def deconstruct(word)
     word.chars.each_slice(4).map { |letter| letter.push }
   end
