dae8e848267553db63677e9aadf67a971e4f2169
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 22:39:42 -0700

    Create digitize method

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -5,8 +5,11 @@ class Compiler
     @alphabet= ("a".."z").to_a << " "
   end
 
+  def digitize(word)
+    word.downcase.chars.map { |letter| @alphabet.index(letter) }
+  end
+
   def deconstruct(word)
     word.chars.each_slice(4).map { |letter| letter.push }
   end
-
 end
