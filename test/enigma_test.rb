bfb5d5f39e7d9a66a729b69e9c1864707a2bb939
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-29 12:16:03 -0700

    Removed tests that used logic that was removed from class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,32 +5,20 @@ require 'minitest/pride'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
-  
+
   def test_it_exists
-    enigma = Enigma.new("message", "key", "date")
+    enigma = Enigma.new
     assert_instance_of Enigma, enigma
   end
 
-  def test_it_has_information
-    enigma = Enigma.new("message", "key", "date")
-    assert_equal "message", enigma.message
-    assert_equal "key", enigma.key
-    assert_equal "date", enigma.date
-  end
-
-  def test_it_has_default_values
-  enigma1 = Enigma.new("Message to Encrypt")
-  assert_equal "Message to Encrypt", enigma1.message
-  assert_equal "default", enigma1.key
-  assert_equal "000000", enigma1.date
-  end
-
-  def test_it_will_generate_a_random_five_digit_key
-    enigma1 = Enigma.new("Message to Encrypt")
-
-    assert_equal true, enigma1.key.length == 5
-    assert_equal true, enigma1.key >= 0
-    assert_equal true, enigma1.key <= 99999
+  def test_it_can_encrypt_messages
+    enigma = Enigma.new
+      expected = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+  assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
 
   end
 
