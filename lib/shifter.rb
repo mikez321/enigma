c956b6d13d47c01d05320f676fc65e516a7ddfe2
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 20:41:58 -0700

    Add create shift to shifter class

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -10,10 +10,14 @@ class Shifter
     keys = []
     keys_from_input.each_with_index do |num, index|
       if index < (keys_from_input.length) - 1
-      keys << (num + keys_from_input[index + 1]).to_i
+      keys << (num + keys_from_input[index + 1])
       end
     end
     keys
+  end
+
+  def create_shift
+    (@date.to_i ** 2).to_s.split("")[-4..-1]
   end
 
 end
