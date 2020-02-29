81a889d8e26762c38fbc50471191a2e8b1bd1b35
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-29 13:03:22 -0700

    Create encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,19 @@
 require './lib/shifter'
+require './lib/compiler'
 class Enigma
 
 
   def encrypt(message, key, date)
     shifter = Shifter.new(key, date)
-    keys = shifter.create_keys
-    shift = shifter.create_shift
-    compiler = Compiler.new()
+    compiler_shift = shifter.compiler_ready_shift
+    compiler = Compiler.new(message, compiler_shift)
+    message = compiler.scramble
+
+    output = {
+      encryption: message,
+      key: key,
+      date: date
+    }
   end
 
 
