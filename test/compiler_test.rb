3fe8824857bb0487cae9e9b36f7ecfcd5f074894
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-28 21:49:57 -0700

    Create a message attr reader method test

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -7,10 +7,14 @@ require './lib/compiler'
 class CompilerTest < Minitest::Test
 
   def setup
-    @compiler = Compiler.new
+    @compiler = Compiler.new("hello world")
   end
 
   def test_it_exists
     assert_instance_of Compiler, @compiler
+  end
+
+  def test_it_has_a_message
+    assert_equal "hello world", @compiler.message
   end
 end
