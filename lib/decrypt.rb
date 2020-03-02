b0db1c4905a34839d21220f0476d66c7a1cee6ba
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-02 10:36:46 -0700

    Fix output message for decrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,4 +3,4 @@ encrypted_message = File.open(ARGV[0]).read.chomp
 enigma = Enigma.new
 decoded_message = enigma.decrypt(encrypted_message, ARGV[2], ARGV[3])
 File.write(ARGV[1], decoded_message[:decryption])
-puts "Created #{ARGV[1]} with the key #{enigma.key} and date #{enigma.date}"
+puts "Created #{ARGV[1]} with the key #{ARGV[2]} and date #{ARGV[3]}"
