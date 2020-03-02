27647b7958b7f2673ea6271c5008157afa030bb6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-02 14:09:15 -0700

    Rewrite encrypt and decrypt runners to not use instance vars

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,6 +1,15 @@
 require './lib/enigma'
 new_message = File.open(ARGV[0]).read.chomp
-enigma = Enigma.new(ARGV[2], ARGV[3])
-coded_message = enigma.encrypt(new_message, enigma.key, enigma.date)
+enigma = Enigma.new
+
+if ARGV[2] == nil
+  ARGV[2] = enigma.key
+end
+
+if ARGV[3] == nil
+  ARGV[3] = enigma.date
+end
+
+coded_message = enigma.encrypt(new_message, ARGV[2], ARGV[3])
 File.write(ARGV[1], coded_message[:encryption])
-puts "Created #{ARGV[1]} with the key #{enigma.key} and date #{enigma.date}"
+puts "Created #{ARGV[1]} with the key #{ARGV[2]} and date #{ARGV[3]}"
