40d9ec2d8f57b6a3286f9a248ecf089cfa1fffe5
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-02 14:42:24 -0700

    Remove validator methods into validator class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,46 +1,14 @@
 require './lib/shifter'
 require './lib/compiler'
 require 'date'
-require './modules/referenceable'
+
 class Enigma
-  include Referenceable
-
-  def date
-    if @date == nil || @date.chars.map { |num| numbers.include?(num) }.all?(false) || @date.length !=6
-      @date = Date.today.strftime("%d%m%y")
-    end
-    # @given_date_used = false
-    # @date
-  end
-
-  def key
-    if @key == nil || @key.chars.map { |num| numbers.include?(num) }.all?(false) || @key.length != 5
-    default_key = rand(0..99999).to_s
-      until default_key.length == 5
-        default_key = default_key.to_s.chars.unshift("0").join("")
-      end
-      @key = default_key
-    end
-    # @given_key_used = false
-    # @key
-  end
-
-  # def error_message
-  #   if @given_key_used == false
-  #     puts "WARNING: The given key was invalid or missing."
-  #   end
-  #
-  #   if @given_date_used == false
-  #     puts "WARNING: The given date was invalid or missing."
-  #   end
-  # end
 
   def encrypt(message, optional_key = key, optional_date = date)
     shifter = Shifter.new(optional_key, optional_date)
     compiler_shift = shifter.compiler_ready_shift
     compiler = Compiler.new(message, compiler_shift)
     cipher = compiler.scramble
-    # error_message
 
     output = {
       encryption: cipher,
@@ -51,8 +19,7 @@ class Enigma
 
   def decrypt(ciphertext, optional_key = key, optional_date = date)
     shifter = Shifter.new(optional_key, optional_date)
-
-    ## here's the difference
+    
     compiler_shift = shifter.compiler_ready_shift.transform_values do
        |value| value * -1
      end
