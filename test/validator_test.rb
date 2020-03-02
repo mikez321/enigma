1e59547fdc767d4a8f8c44b4c61790101b59d8a7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-02 14:41:20 -0700

    Complete move of validators from enigma to valdiator test

--- test/validator_test.rb
+++ test/validator_test.rb
@@ -1,37 +1,52 @@
 require './lib/validator'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 
 class ValidatorTest < Minitest::Test
 
-  def test_if_an_incorrect_key_is_assigned_it_will_assign_a_random_key
-    skip
-    #this is an error checking test
-    enigma1 = Enigma.new("ABCD")
-    enigma1.stubs(:rand).returns("339")
-    assert_equal "00339", enigma1.key
-
-    enigma2 = Enigma.new("00.912")
-    enigma2.stubs(:rand).returns("339")
-    assert_equal "00339", enigma2.key
-
-    enigma3 = Enigma.new("12")
-    enigma3.stubs(:rand).returns("339")
-    assert_equal "00339", enigma3.key
+  def test_it_exists
+    validator = Validator.new("02715", "030220")
+    assert_instance_of Validator, validator
   end
-  
+
+  def test_if_no_key_is_assigned_it_is_a_random_5_digit_number
+    validator = Validator.new
+    validator.stubs(:rand).returns("339")
+    assert_equal "00339", validator.key
+  end
+
+  def test_if_an_incorrect_key_is_assigned_it_will_assign_a_random_key
+    validator1 = Validator.new("ABCD")
+    validator1.stubs(:rand).returns("339")
+    assert_equal "00339", validator1.key
+
+    validator2 = Validator.new("00.912")
+    validator2.stubs(:rand).returns("339")
+    assert_equal "00339", validator2.key
+
+    validator3 = Validator.new("12")
+    validator3.stubs(:rand).returns("339")
+    assert_equal "00339", validator3.key
+  end
+
+  def test_it_assigns_todays_date_if_it_is_not_given_one
+    Date.stubs(:today).returns(Date.new(2020, 2, 29))
+    #For the purpose of this test, today's date is Feb 29, 2020
+      validator = Validator.new
+      assert_equal "290220", validator.date
+  end
+
   def test_if_an_incorrect_date_is_assigned_it_will_assign_a_random_date
-    skip
-    #this is an error checking method
     Date.stubs(:today).returns(Date.new(2020, 2, 29))
 
-    enigma1 = Enigma.new("02715", "895")
-    assert_equal "290220", enigma1.date
+    validator1 = Validator.new("02715", "895")
+    assert_equal "290220", validator1.date
 
-    enigma2 = Enigma.new("02715")
-    assert_equal "290220", enigma2.date
+    validator2 = Validator.new("02715", "8.11")
+    assert_equal "290220", validator2.date
 
-    enigma3 = Enigma.new("02715")
-    assert_equal "290220", enigma3.date
+    validator3 = Validator.new("02715", "Josh")
+    assert_equal "290220", validator3.date
   end
 end
