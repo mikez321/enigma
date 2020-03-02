d73ca4c25d231e9d99262bc23af4a58b1fb85128
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-02 10:27:58 -0700

    Edit runners to follow refactored enigma class

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,6 +1,6 @@
 require './lib/enigma'
 encrypted_message = File.open(ARGV[0]).read.chomp
-enigma = Enigma.new(encrypted_message)
+enigma = Enigma.new
 decoded_message = enigma.decrypt(encrypted_message, ARGV[2], ARGV[3])
 File.write(ARGV[1], decoded_message[:decryption])
 puts "Created #{ARGV[1]} with the key #{enigma.key} and date #{enigma.date}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,6 +1,6 @@
 require './lib/enigma'
 new_message = File.open(ARGV[0]).read.chomp
-enigma = Enigma.new(new_message, ARGV[2], ARGV[3])
+enigma = Enigma.new(ARGV[2], ARGV[3])
 coded_message = enigma.encrypt(new_message, enigma.key, enigma.date)
 File.write(ARGV[1], coded_message[:encryption])
 puts "Created #{ARGV[1]} with the key #{enigma.key} and date #{enigma.date}"
