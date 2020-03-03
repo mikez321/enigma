037489bb0c3426dd7eea130d22c52b6cc8a181b4
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-02 21:44:04 -0700

    Add methods to set key and date in shifter class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,22 +4,22 @@ require 'date'
 
 class Enigma
 
-  def encrypt(message, optional_key = key, optional_date = date)
+  def encrypt(message, optional_key = nil, optional_date = nil)
     shifter = Shifter.new(optional_key, optional_date)
+    shifter.set_key
+    shifter.set_date
     compiler_shift = shifter.compiler_ready_shift
     compiler = Compiler.new(message, compiler_shift)
     cipher = compiler.scramble
 
-    output = {
-      encryption: cipher,
-      key: optional_key,
-      date: optional_date
-    }
+    {encryption: cipher, key: shifter.key_to_use, date: shifter.date_to_use}
   end
 
-  def decrypt(ciphertext, optional_key = key, optional_date = date)
+  def decrypt(ciphertext, optional_key = nil, optional_date = nil)
     shifter = Shifter.new(optional_key, optional_date)
-    
+    shifter.set_key
+    shifter.set_date
+
     compiler_shift = shifter.compiler_ready_shift.transform_values do
        |value| value * -1
      end
@@ -27,11 +27,7 @@ class Enigma
     compiler = Compiler.new(ciphertext, compiler_shift)
     message = compiler.scramble
 
-    output = {
-      decryption: message,
-      key: optional_key,
-      date: optional_date
-    }
+    {decryption: message, key: shifter.key_to_use, date: shifter.date_to_use}
   end
 
 end
