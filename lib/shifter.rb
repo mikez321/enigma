9bf5d538a6c5cef7ef32f075c08752c9bf23d35b
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-02 20:47:22 -0700

    Integrate validation class into shifter class

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -1,22 +1,34 @@
+require './lib/validator'
 require './modules/referenceable'
 class Shifter
   include Referenceable
-  attr_reader :key, :date
-  def initialize(key, date)
-    @key = key
-    @date = date
+  def initialize(passed_key, passed_date)
+    @passed_key = passed_key
+    @passed_date = passed_date
+  end
+
+  def key
+    validator = Validator.new(@passed_key, @passed_date)
+    validator.validate_start_key
+    validator.valid_key
+  end
+
+  def date
+    validator = Validator.new(@passed_key, @passed_date)
+    validator.validate_start_date
+    validator.valid_date
   end
 
   def create_key_shift
-    @key.chars.each_with_index.map do |num, index|
-      if index < (@key.chars.length) - 1
-        (num + @key.chars[index + 1]).to_i
+    key.chars.each_with_index.map do |num, index|
+      if index < (key.chars.length) - 1
+        (num + key.chars[index + 1]).to_i
       end
     end.compact
   end
 
   def date_squared
-    (@date.to_i ** 2).to_s
+    (date.to_i ** 2).to_s
   end
 
   def create_date_shift
